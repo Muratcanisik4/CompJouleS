a68f4786d126b802cc2ac5c60ed83be7976c625a
Muratcanisik4 <muratisik44@icloud.com>
2023-12-04 18:20:11 -0800

    Initial commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CompJouleS'
+copyright = '2023, Murat'
+author = 'Murat'
 
 release = '0.1'
 version = '0.1.0'
